d73dd5dddeb298b111c8271dceeddca57d427168
Saitem <gamesshooter42@gmail.com>
2020-03-14 00:53:32 +0200

    some changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
 import Header from '@/components/layout/Header'
 
 export default {
-  components: {Header}
+  components: { Header }
 }
 </script>
 
@@ -19,28 +19,10 @@ export default {
 $image-path: '~@/../mdb/mdbvue/img';
 @import '~@/../mdb/mdbvue/scss/mdb-free.scss';
 
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
 body{
     background-color: rgba(222, 231, 235, 0.733);
     font-family: 'Roboto', sans-serif;
 
 }
-// #nav {
-//   padding: 30px;
 
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 </style>
